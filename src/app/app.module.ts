90814773f108167fe85e94eb3c8ddc683f7116c0
snefedov <snefedov@rambler-co.ru>
2020-03-18 17:17:25 +0300

    tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
-import { ActionComponent } from './action/action.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -13,9 +11,7 @@ import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ListComponent,
-    ActionComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
